22c6e88650acf75ee65c3ed333fa2303ef4f93d9
sonamtmg96 <sonam.tmg96@gmail.com>
2020-01-12 23:22:23 -0500

    created histogram for ages

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MortgageApplicationAcceptor
 Program that determines whether a mortgage application should be accepted.
+
+pip install -r requirements.txt for the necessary packages
--- main.py
+++ main.py
@@ -6,6 +6,9 @@
 """
 
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
+import collections
 
 
 def read_application(document):
@@ -29,3 +32,16 @@ if __name__ == "__main__":
     split_point = int(.80*len(applications))
     train_applications = applications[:split_point]
     eval_applications = applications[split_point:]
+
+    ages = {}
+    for application in applications:
+        age = application[1]
+        if age in ages:
+            ages[age] += 1
+        else:
+            ages[age] = 1
+        
+    ordered_ages = collections.OrderedDict(sorted(ages.items()))
+
+    plt.bar(list(ordered_ages.keys()), ordered_ages.values(), color='g')
+    plt.show()
